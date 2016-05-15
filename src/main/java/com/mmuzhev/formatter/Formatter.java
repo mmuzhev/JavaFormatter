b7054f2f99008dd30596a132556efbba4947dee2
Mikhail Muzhev <mmuzhev@mail.ru>
2016-05-15 19:34:49 +0600

    Add cases '(' and ')'. Add formatting of logical operations.

--- src/main/java/com/mmuzhev/formatter/Formatter.java
+++ src/main/java/com/mmuzhev/formatter/Formatter.java
@@ -6,6 +6,8 @@ package com.mmuzhev.formatter;
 public class Formatter implements FormatterInterface {
     private int spaceCount = 0;
     private boolean enter = false;
+    private int ForLogic = 0;
+    private char PreviousLogicalSymbol = ' ';
 
     /**
      * format symbol to Java-code view
@@ -15,22 +17,34 @@ public class Formatter implements FormatterInterface {
     public String format(char symbol) {
         StringBuilder result = new StringBuilder();
         char symbol1 = ' ';
+        char symbol2 = ' ';
 
         // few similar cases in one
-        if (symbol == '=' || symbol == '+' || symbol == '-' || symbol == '*' || symbol == '(' || symbol == ')') {
+        if (symbol == '=' || symbol == '+' || symbol == '-' || symbol == '*' ) {
 
             symbol1 = symbol;
             symbol = '=';
         }
+        if ( symbol == '&' || symbol == '|' ) {
+
+            symbol2 = symbol;
+            symbol = '&';
+        }
+        else if(ForLogic == 1) {
+            result.append(PreviousLogicalSymbol);
+            ForLogic = 0;
+        }
 
         switch (symbol) {
             case ';': {
+                ForLogic = 0;
                 result.append(symbol);
                 result.append("\n");
                 enter = true;
                 break;
             }
             case '{' : {
+                ForLogic = 0;
                 result.append(forSpace(1));
                 result.append(symbol);
                 spaceCount += 4;
@@ -39,12 +53,14 @@ public class Formatter implements FormatterInterface {
                 break;
             }
             case '=' : {
+                ForLogic = 0;
                 result.append(forSpace(1));
                 result.append(symbol1);
                 result.append(forSpace(1));
                 break;
             }
             case '}' : {
+                ForLogic = 0;
                 if (spaceCount >= 4) {
                     spaceCount -= 4;
                     result.substring(0);
@@ -62,6 +78,30 @@ public class Formatter implements FormatterInterface {
                     result.append("\n");
                     break;
                 }
+            }
+            case '(' : {
+                result.append(forSpace(1));
+                result.append(symbol);
+                break;
+            }
+            case ')' : {
+                result.append(symbol);
+                result.append(forSpace(1));
+                break;
+            }
+            case '&' : {
+                if (ForLogic == 1) {
+                    result.append(forSpace(1));
+                    result.append(symbol2);
+                    result.append(symbol2);
+                    result.append(forSpace(1));
+                    ForLogic = 0;
+                }
+                else {
+                    ForLogic = 1;
+                    PreviousLogicalSymbol = symbol2;
+                }
+                break;
             }
             default:{
                 if(enter){
